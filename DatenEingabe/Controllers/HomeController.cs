da8e13fb4ec9bde019c92d4467f0409f0659ad5c
GinaXYZ <qiiina122@hotmail.de>
2025-09-23 12:59:28 +0200

    Initial commit for DatenEingabe

---
+++ DatenEingabe/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DatenEingabe.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult DatenEingabe()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DatenEingabe(string Vorname, string Nachname, DateOnly Geburtsdatum)
+        {
+
+            return RedirectToAction("Index");
+        }
+    }
+}
